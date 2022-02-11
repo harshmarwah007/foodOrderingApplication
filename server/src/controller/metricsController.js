e69d04ecc3a828a8649253a8f9ad191609cb3980
harshmarwah007 <harshmarwah@Harshs-MacBook-Pro.local>
2022-02-11 13:46:24 +0530

    metrics controller and modal bug resolved

--- server/src/controller/metricsController.js
+++ server/src/controller/metricsController.js
@@ -2,13 +2,30 @@ const foodOrders = require("../models/FoodOrders");
 
 
 const getAllMetrics = async (req,res) =>{
-    const orderMetrics = await foodOrders.aggregate([
+    
+
+    const topSelling = await foodOrders.aggregate([
         {$unwind:"$dishList"},
         {$group:{_id:"$dishList.name",qty:{$sum:"$dishList.qty"}}},
       {$sort:{qty:-1}}
     ]).limit(1)
 
-    
+
+    const count = await foodOrders.aggregate(
+        [ 
+            { "$group":  { _id: "$customerName", "count": { "$sum": 1 } } },
+            {$sort:{count:-1}}
+        ],  function(err, results) {
+                // Do something with the results
+            }
+    )
+
+    const orderMetrics ={
+        topSelling:topSelling,
+        count:count
+
+        
+    }
     console.log(orderMetrics);
     res.json({
         orderMetrics
